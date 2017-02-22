44093f9682edea237cbe316488332f9806c9cb92
Jaroslav Brychta <brychta.jaroslav@gmail.com>
2017-02-22 20:00:07 +0100

    label added

--- src/Form.php
+++ src/Form.php
@@ -5,18 +5,18 @@ namespace Brychtaj;
 class Form extends FormFactory
 {
 
-	public static function field($name = null, $value = null)
-	{
-		echo self::render("text", "form-control", $name, $value);
-	}
+    public static function field($name = null, $value = null)
+    {
+        echo self::render("text", "form-control", $name, $value);
+    }
 
-	public static function password()
-	{
-		echo self::render("password", "form-control", "password");
-	}
+    public static function password()
+    {
+        echo self::render("password", "form-control", "password");
+    }
 
-	public static function submit($name = null, $value = "Odeslat")
-	{
-		echo self::render("submit", "btn", $name, $value);
-	}
+    public static function submit($name = null, $value = "Odeslat")
+    {
+        echo self::render("submit", "btn", $name, $value);
+    }
 }
--- src/FormFactory.php
+++ src/FormFactory.php
@@ -4,22 +4,29 @@ namespace Brychtaj;
 
 class FormFactory
 {
-	protected function render($type, $class, $name = null, $value = null)
-	{
-		return "
-			<div class='form-group'>
-				<input type='$type' name='$name' value='$value' class='$class'>
-			</div>
-		";
-	}
+    protected function render($type, $class, $name = null, $value = null)
+    {
+        return "
+            <div class='form-group'>
+                <input type='$type' name='$name' value='$value' class='$class'>
+            </div>
+        ";
+    }
 
-	public static function open($action = null, $method = null)
-	{
-		echo "<form action='$action' method='$method'>";
-	}
+    public static function open($action = null, $method = null)
+    {
+        echo "<form action='$action' method='$method'>";
+    }
 
-	public static function close()
-	{
-		echo "<form>";
-	}
+    public static function close()
+    {
+        echo "<form>";
+    }
+
+    public static function label($name, $for = null)
+    {
+        echo "
+            <label for='$for' class='control-llabel'>$name</label>
+        ";
+    }
 }
